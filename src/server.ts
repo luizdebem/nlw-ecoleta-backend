f6e1ca7446af761292b0d0d1d46a15d4b26c0960
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-06-02 19:28:00 -0300

    db setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.json([
-    'Diego', 'Cleiton', 'Robson', 'Josias'
-  ]);
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333);
