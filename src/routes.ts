2123a5eb03c42a43fb640eb1f6d153a1056952c5
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-06-05 13:26:13 -0300

    Validation with Celebrate

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import { celebrate, Joi } from 'celebrate'; // validação de dados
+
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
 
@@ -14,7 +16,18 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image') ,pointsController.create);
+routes.post('/points', upload.single('image'), celebrate({ body: Joi.object().keys({
+  name: Joi.string().required(),
+  email: Joi.string().required().email(),
+  whatsapp: Joi.string().required(),
+  lat: Joi.number().required(),
+  lon: Joi.number().required(),
+  city: Joi.string().required(),
+  uf: Joi.string().required().max(2),
+  items: Joi.string().required()
+}) }, {
+  abortEarly: false // valida tudo ao mesmo tempo
+}), pointsController.create);
 routes.get('/points/:id', pointsController.show);
 routes.get('/points', pointsController.index);
 
